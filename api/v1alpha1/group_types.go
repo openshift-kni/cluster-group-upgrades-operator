7079c79555b123ad7aa625497875174ad43f0706
Ricardo Carrillo Cruz <ricarril@redhat.com>
2021-06-23 08:55:40 +0200

    Add Conditions to Group status

--- api/v1alpha1/group_types.go
+++ api/v1alpha1/group_types.go
@@ -53,6 +53,7 @@ type GroupSpec struct {
 type GroupStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions"`
 }
 
 //+kubebuilder:object:root=true
