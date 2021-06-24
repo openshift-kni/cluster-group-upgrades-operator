defe8ba53cd5927d90eba3f96bc9daf70392303a
Ricardo Carrillo Cruz <ricarril@redhat.com>
2021-06-24 12:03:17 +0200

    Add RBAC permissions to Site for ACM objects management

--- controllers/site_controller.go
+++ controllers/site_controller.go
@@ -41,6 +41,10 @@ type SiteReconciler struct {
 //+kubebuilder:rbac:groups=ran.openshift.io,resources=sites,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ran.openshift.io,resources=sites/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=ran.openshift.io,resources=sites/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps.open-cluster-management.io,resources=placementrules,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=policy.open-cluster-management.io,resources=placementbindings,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=policy.open-cluster-management.io,resources=policies,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=cluster.open-cluster-management.io,resources=managedclusters,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
