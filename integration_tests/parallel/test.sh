674a67d870df6112f139086cdae3d320150f9b19
Ricardo Carrillo Cruz <ricarril@redhat.com>
2021-07-13 13:23:23 +0200

    Fix integration tests

--- integration_tests/parallel/test.sh
+++ integration_tests/parallel/test.sh
@@ -18,17 +18,17 @@ echo "Sleeping a bit"
 sleep 5
 
 echo "Testing placement rules generated"
-diff site1-placement-rule.spec <(oc get placementrules.apps.open-cluster-management.io site1-placement-rule -ojson | jq -Sc '.spec') 
-diff site2-placement-rule.spec <(oc get placementrules.apps.open-cluster-management.io site2-placement-rule -ojson | jq -Sc '.spec') 
-diff group1-batch-1-placement-rule.spec <(oc get placementrules.apps.open-cluster-management.io group1-batch-1-placement-rule -ojson | jq -Sc '.spec')
+diff site1-placement-rule.spec <(oc get placementrules.apps.open-cluster-management.io site1 -ojson | jq -Sc '.spec') 
+diff site2-placement-rule.spec <(oc get placementrules.apps.open-cluster-management.io site2 -ojson | jq -Sc '.spec') 
+diff group1-batch-1-placement-rule.spec <(oc get placementrules.apps.open-cluster-management.io group1-batch-1 -ojson | jq -Sc '.spec')
 
 echo "Testing placement bindings generated"
-diff site1-placement-binding.placementRef <(oc get placementbindings.policy.open-cluster-management.io site1-placement-binding -ojson | jq -Sc '.placementRef') 
-diff site1-placement-binding.subjects <(oc get placementbindings.policy.open-cluster-management.io site1-placement-binding -ojson | jq -Sc '.subjects') 
-diff site2-placement-binding.placementRef <(oc get placementbindings.policy.open-cluster-management.io site2-placement-binding -ojson | jq -Sc '.placementRef') 
-diff site2-placement-binding.subjects <(oc get placementbindings.policy.open-cluster-management.io site2-placement-binding -ojson | jq -Sc '.subjects') 
-diff group1-batch-1-placement-binding.placementRef <(oc get placementbindings.policy.open-cluster-management.io group1-batch-1-placement-binding -ojson | jq -Sc '.placementRef') 
-diff group1-batch-1-placement-binding.subjects <(oc get placementbindings.policy.open-cluster-management.io group1-batch-1-placement-binding -ojson | jq -Sc '.subjects') 
+diff site1-placement-binding.placementRef <(oc get placementbindings.policy.open-cluster-management.io site1 -ojson | jq -Sc '.placementRef') 
+diff site1-placement-binding.subjects <(oc get placementbindings.policy.open-cluster-management.io site1 -ojson | jq -Sc '.subjects') 
+diff site2-placement-binding.placementRef <(oc get placementbindings.policy.open-cluster-management.io site2 -ojson | jq -Sc '.placementRef') 
+diff site2-placement-binding.subjects <(oc get placementbindings.policy.open-cluster-management.io site2 -ojson | jq -Sc '.subjects') 
+diff group1-batch-1-placement-binding.placementRef <(oc get placementbindings.policy.open-cluster-management.io group1-batch-1 -ojson | jq -Sc '.placementRef') 
+diff group1-batch-1-placement-binding.subjects <(oc get placementbindings.policy.open-cluster-management.io group1-batch-1 -ojson | jq -Sc '.subjects') 
 
 echo "Testing policies generated"
 diff common-group1-batch-1-policy.spec <(oc get policies.policy.open-cluster-management.io common-group1-batch-1-common-namespace -ojson | jq -Sc '.spec')
