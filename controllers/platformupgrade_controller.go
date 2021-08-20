091f94dd6c9038162fa35383a770ea0fa2d71779
Ricardo Carrillo Cruz <ricarril@redhat.com>
2021-08-20 10:56:08 +0200

    Fix import paths to point to new openshift-kni repo

--- controllers/platformupgrade_controller.go
+++ controllers/platformupgrade_controller.go
@@ -37,7 +37,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
-	ranv1alpha1 "github.com/redhat-ztp/cluster-group-lcm/api/v1alpha1"
+	ranv1alpha1 "github.com/openshift-kni/cluster-group-upgrades-operator/api/v1alpha1"
 )
 
 const policyTemplate = `
