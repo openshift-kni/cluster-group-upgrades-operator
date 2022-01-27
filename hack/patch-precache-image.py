e9bf5c49cd4a1e8b4944d4bc5cf25445c1afce95
Vitaly Grinberg <vgrinber@redhat.com>
2022-01-27 18:55:09 +0200

    Update precache image tag in csv
    Automatically patch pre-cache image tag in csv
    to the operator version.
    
    Signed-off-by: Vitaly Grinberg <vgrinber@redhat.com>

---
+++ hack/patch-precache-image.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import yaml
+import sys
+
+file = sys.argv[1]
+pull = sys.argv[2]
+with open(file,'r') as f:
+    obj = yaml.safe_load(f)
+for item in obj.get('spec',{}).get('relatedImages',{}):
+    if item.get('name','') == "pre-caching-workload":
+        item['image'] = pull
+    break
+with open(file,'w') as f:
+    yaml.dump(obj, f)
