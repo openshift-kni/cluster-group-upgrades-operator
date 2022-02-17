8aa03760acf548116fabb813fbc857e500a02a3b
Angie Wang <angwang@redhat.com>
2022-02-17 10:27:27 -0500

    Filter out the copied child policies
    - Move the GetChildPolicies function to a utility function and
      make sure the retrieved child policies are not from copied policies
    - Remove the getPoliciesForNamespace function as it's not being used
    - Add a testcase to verify the child policy of the copied policy is
      not added in the list of managedPolicies in the generated CGU
    
    Signed-off-by: Angie Wang <angwang@redhat.com>

--- controllers/utils/policy_util.go
+++ controllers/utils/policy_util.go
@@ -9,6 +9,35 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// GetChildPolicies gets the child policies for a list of clusters
+func GetChildPolicies(ctx context.Context, c client.Client, clusters []string) ([]policiesv1.Policy, error) {
+	var childPolicies []policiesv1.Policy
+
+	for _, clusterName := range clusters {
+		policies := &policiesv1.PolicyList{}
+		if err := c.List(ctx, policies, client.InNamespace(clusterName)); err != nil {
+			return nil, err
+		}
+
+		for _, policy := range policies.Items {
+			labels := policy.GetLabels()
+			if labels == nil {
+				continue
+			}
+			// Skip if it's the child policy of a copied policy.
+			if _, ok := labels["openshift-cluster-group-upgrades/clusterGroupUpgrade"]; ok {
+				continue
+			}
+			// If we can find the child policy specific label, add the child policy name to the list.
+			if _, ok := labels[ChildPolicyLabel]; ok {
+				childPolicies = append(childPolicies, policy)
+			}
+		}
+	}
+
+	return childPolicies, nil
+}
+
 // DeletePolicies deletes Policies
 func DeletePolicies(ctx context.Context, c client.Client, ns string, labels map[string]string) error {
 	listOpts := []client.ListOption{
