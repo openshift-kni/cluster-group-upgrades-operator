672a7790aa30cf8535520db2d02df8beaaf43982
Don Penney <dpenney@redhat.com>
2022-04-01 08:42:39 -0400

    Enable shellcheck for all .sh scripts
    Dropped the previously excluded directories from the shellcheck linter
    test and addressed or disabled existing warnings. This ensures that
    future updates to scripts must cleanly pass shellcheck.
    
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- pre-cache/copy-env.sh
+++ pre-cache/copy-env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cwd="${cwd:-/opt/precache}"
-. $cwd/common
+# shellcheck source=pre-cache/common
+source $cwd/common
 
 # Copy podman dependencies to the container. Podman itself runs off the host in the
 # container by adding /host/usr/bin to the path
@@ -23,6 +24,7 @@ done
 cp /host/etc/containers/policy.json /etc/containers/policy.json
 cp /host/etc/containers/registries.conf /etc/containers/registries.conf || true
 cp -a /host/etc/docker /etc/ || true
+# shellcheck disable=SC2015
 cp -a /host/etc/pki/ca-trust/source/anchors /etc/pki/ca-trust/source/ && update-ca-trust || true
 cp -a /host/etc/pki/ca-trust/extracted/pem /etc/pki/ca-trust/extracted/ || true
 [ ! -d "$podman_pull_path" ] && mkdir -p "$podman_pull_path"
