c0119f9148c66fe88c56f3da423229dcdb6bb0fb
Steven Skeard <sskeard@redhat.com>
2022-07-19 16:13:03 -0400

    Bug 2106114: Cherry pick fix for Batch times out faster than configured timeout (#259)
    This was originally submitted under Bug 2105992 however due to the differences
    in the code between 4.10 and main could not be cherry picked automatically.

---
+++ controllers/utils/batches_test.go
@@ -0,0 +1,117 @@
+package utils
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBatchTimeout(t *testing.T) {
+
+	type (
+		BatchTimeoutTestInputs struct {
+			timeoutMinutes        int
+			currentBatchStartTime time.Time
+			cguStartTime          time.Time
+			numBatches            int
+			currentBatch          int
+		}
+	)
+
+	testcases := []struct {
+		inputs   BatchTimeoutTestInputs
+		expected time.Duration
+		name     string
+	}{
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        100,
+				currentBatchStartTime: time.Unix(1657000000, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            1,
+				currentBatch:          0,
+			},
+			expected: time.Duration(100 * time.Minute),
+			name:     "Base case 1",
+		},
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        200,
+				currentBatchStartTime: time.Unix(1657000000, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            2,
+				currentBatch:          0,
+			},
+			expected: time.Duration(100 * time.Minute),
+			name:     "Base case 2",
+		},
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        200,
+				currentBatchStartTime: time.Unix(1657006000, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            2,
+				currentBatch:          1,
+			},
+			expected: time.Duration(100 * time.Minute),
+			name:     "Base case 3",
+		},
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        240,
+				currentBatchStartTime: time.Unix(1657000000, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            5,
+				currentBatch:          0,
+			},
+			expected: time.Duration(48 * time.Minute),
+			name:     "Realistic case 1",
+		},
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        240,
+				currentBatchStartTime: time.Unix(1657002400, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            5,
+				currentBatch:          1,
+			},
+			expected: time.Duration(50 * time.Minute),
+			name:     "Realistic case 2",
+		},
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        240,
+				currentBatchStartTime: time.Unix(1657006000, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            5,
+				currentBatch:          4,
+			},
+			expected: time.Duration(140 * time.Minute),
+			name:     "Realistic case 3",
+		},
+		{
+			inputs: BatchTimeoutTestInputs{
+				timeoutMinutes:        100,
+				currentBatchStartTime: time.Unix(1657003000, 0),
+				cguStartTime:          time.Unix(1657000000, 0),
+				numBatches:            100,
+				currentBatch:          99,
+			},
+			expected: time.Duration(50 * time.Minute),
+			name:     "Edge case 1",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := CalculateBatchTimeout(
+				tc.inputs.timeoutMinutes,
+				tc.inputs.numBatches,
+				tc.inputs.currentBatch,
+				tc.inputs.currentBatchStartTime,
+				tc.inputs.cguStartTime)
+			assert.Equal(t, tc.expected, actual, "The expected and actual timeout should be the same.")
+		})
+	}
+}
