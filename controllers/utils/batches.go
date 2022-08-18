d9a2a7f25f1e5b94ad5c697e3412b36028b84666
Steven Skeard <sskeard@redhat.com>
2022-08-18 11:06:51 -0400

    [release-4.10] Bug 2117228: Fix batch timeout calculation for first batch

--- controllers/utils/batches.go
+++ controllers/utils/batches.go
@@ -11,9 +11,15 @@ func CalculateBatchTimeout(timeoutMinutes, numBatches, currentBatch int, current
 	// It is important to include the current batch here so that we don't let the entire timeout be consumed by a single batch that gets stuck
 	remainingTime := float64(timeoutMinutes)*float64(time.Minute) - float64(currentBatchStartTime.Sub(cguStartTime).Nanoseconds())
 
-	// The number of batches will always be at least 1, and currentBatch is indexed from 0, so there should never be a <1 result here
-	remainingBatches := numBatches - currentBatch
+	// Because the current batch index automatically advances from 0 to 1 on the first loop,
+	// then we need to subtract one when using it in here to ensure the math is correct
+	remainingBatches := numBatches - (currentBatch - 1)
 
+	// If this is the last batch then just return all the remaining time.
+	// This also makes sure there is no division by zero below.
+	if remainingBatches <= 1 {
+		return time.Duration(remainingTime)
+	}
 	// The current batch's timeout shall be the remaining time divided by the number of batches remaining
 	// This is to ensure we are giving each batch as much time as possible within the remaining allotment
 	currentBatchTimeout := time.Duration(remainingTime / float64(remainingBatches))
