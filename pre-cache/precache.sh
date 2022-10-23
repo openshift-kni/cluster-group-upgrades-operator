57ade97d4107b72766a1a09bb68967fbcb356f48
Vitaly Grinberg <vgrinber@redhat.com>
2022-10-23 07:33:50 +0300

    make precaching compatible across versions
    This is a backport fo precaching compatibility fixes introduced in 4.12
    and 4.11. It removes obsolete code causing incompatibilities between
    ubi8-based containers and underlying RHCOS hosts, fully aligning pre-caching
    implementation between the operator releases. As a result, TALM 4.10
    can pre-cache 4.12 hosts and vice versa.
    
    Signed-off-by: Vitaly Grinberg <vgrinber@redhat.com>

--- pre-cache/precache.sh
+++ pre-cache/precache.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-# Pull spec extraction is done in the container due to opm<->selinux issues
-/opt/precache/copy-env.sh
-/opt/precache/release
-/opt/precache/olm 
-
-# Image pull is done on the host using "chroot /host"
-cp /tmp/images.txt /host/tmp/
 rm -rf /host/tmp/precache
 cp -a /opt/precache /host/tmp/
+cp -rf /etc/config /host/tmp/precache/config
+chroot /host /tmp/precache/release
+chroot /host /tmp/precache/olm
 chroot /host /tmp/precache/pull
