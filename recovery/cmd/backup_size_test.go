0f6de706a82357438271fdddcfb9ed2cb83960e6
Sabbir Hasan <sahasan@redhat.com>
2022-10-31 16:51:08 +0100

    test-cases expanded for backup size
    Signed-off-by: Sabbir Hasan <sahasan@redhat.com>

--- recovery/cmd/backup_size_test.go
+++ recovery/cmd/backup_size_test.go
@@ -15,34 +15,58 @@ func TestCompare(t *testing.T) {
 		name                string
 	}{
 		{
-			estimated: 80.00 * 1024 * 1024 * 1024,
-			freeDisk:  100.00 * 1024 * 1024 * 1024,
+			estimated: 80.00 * 1024 * 1024 * 1024,  // 80 GiB
+			freeDisk:  100.00 * 1024 * 1024 * 1024, // 100 GiB
 			expected:  true,
 			name:      "higher disk space in same unit",
 		},
 		{
-			estimated: 800.00 * 1024 * 1024 * 1024,
-			freeDisk:  1.00 * 1024 * 1024 * 1024 * 1024,
+			estimated: 800.00 * 1024 * 1024 * 1024,      // 800 GiB
+			freeDisk:  1.00 * 1024 * 1024 * 1024 * 1024, // 1 TiB
 			expected:  true,
 			name:      "higher disk space in different unit",
 		},
 		{
-			estimated: 800.00 * 1024 * 1024 * 1024,
-			freeDisk:  800.00 * 1024 * 1024 * 1024,
-			expected:  false,
-			name:      "equal disk space",
+			estimated: 400.00 * 1024 * 1024,             // 400 MiB
+			freeDisk:  1.00 * 1024 * 1024 * 1024 * 1024, // 1 TiB
+			expected:  true,
+			name:      "higher disk space in different unit (2 step)",
 		},
 		{
-			estimated: 80.00 * 1024 * 1024 * 1024,
-			freeDisk:  50.00 * 1024 * 1024 * 1024,
+			estimated: 790.00 * 1024 * 1024 * 1024, // 790 GiB
+			freeDisk:  800.00 * 1024 * 1024 * 1024, // 800 GiB
+			expected:  false,
+			name:      "when freedisk is equal to estmated + safetynet",
+		},
+		{
+			estimated: 70.00 * 1024 * 1024 * 1024, // 70 GiB
+			freeDisk:  70.00 * 1024 * 1024 * 1024, // 70 GiB
+			expected:  false,
+			name:      "when freedisk is equal to estimated",
+		},
+		{
+			estimated: 70.00 * 1024 * 1024 * 1024, // 70 GiB
+			freeDisk:  75.00 * 1024 * 1024 * 1024, // 75 GiB
+			expected:  false,
+			name:      "when freedisk is greater than estimated but less than estmated + safetynet combined",
+		},
+		{
+			estimated: 80.00 * 1024 * 1024 * 1024, // 80 GiB
+			freeDisk:  50.00 * 1024 * 1024 * 1024, // 50 GiB
 			expected:  false,
 			name:      "lower disk space in same unit",
 		},
 		{
-			estimated: 80.00 * 1024 * 1024 * 1024,
-			freeDisk:  100.00 * 1024 * 1024,
+			estimated: 80.00 * 1024 * 1024 * 1024, // 80 GiB
+			freeDisk:  100.00 * 1024 * 1024,       // 100 MiB
 			expected:  false,
 			name:      "lower disk space in different unit",
+		},
+		{
+			estimated: 80.00 * 1024 * 1024 * 1024, // 80 GiB
+			freeDisk:  100.00 * 1024,              // 100 KiB
+			expected:  false,
+			name:      "lower disk space in different unit (2 step)",
 		},
 	}
 
