07c0a39038809bfb74e3d5804feb8527ec9b6420
Nahian Pathan <npathan@redhat.com>
2022-12-14 10:54:10 -0500

    OCPBUGS-3779: support only image or version in ClusterVersion when doing ocp precache (#399)
    refactor code to support cases and add unit tests
    
    add http mock reply when trying to make external calls
    
    remove the extra check
    
    update logic to handle when CV cr is not present
    
    improve check for spec and add additional test make sure ClusterVersion is present
    
    add const to where policies are changed, add another test make sure that empty strings are ok and make polcies minimal
    
    make the upstearm test url more streamlined and obvious that no external calls are being made to test

--- controllers/validation.go
+++ controllers/validation.go
@@ -4,19 +4,24 @@ import (
 	"errors"
 
 	ranv1alpha1 "github.com/openshift-kni/cluster-group-upgrades-operator/api/v1alpha1"
-	utils "github.com/openshift-kni/cluster-group-upgrades-operator/controllers/utils"
+	"github.com/openshift-kni/cluster-group-upgrades-operator/controllers/utils"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+// extractOpenshiftImagePlatformFromPolicies validates that there's ClusterVersion policy, validates the content of ClusterVersion and extracts Image if needed
 func (r *ClusterGroupUpgradeReconciler) extractOpenshiftImagePlatformFromPolicies(
 	policies []*unstructured.Unstructured) (string, error) {
 
-	var upstream string
-	var channel string
-	var version string
-	var image string
+	var (
+		upstream                        string
+		channel                         string
+		version                         string
+		image                           string
+		foundClusterVersionGroupVersion bool
+	)
 
+	// validate ClusterVersionGroupVersionKind and keep track to upstream, channel, version, image
 	for _, policy := range policies {
 		objects, err := r.stripPolicy(policy.Object)
 		if err != nil {
@@ -26,66 +31,76 @@ func (r *ClusterGroupUpgradeReconciler) extractOpenshiftImagePlatformFromPolicie
 			kind := object["kind"]
 			switch kind {
 			case utils.ClusterVersionGroupVersionKind().Kind:
-				cvSpec := object["spec"].(map[string]interface{})
-				desiredUpdate, found := cvSpec["desiredUpdate"]
-				if !found {
+				_, foundSpec := object["spec"]
+				if !foundSpec || object["spec"] == nil {
 					continue
 				}
-				desiredUpdateImage, found := desiredUpdate.(map[string]interface{})["image"]
-				if found && desiredUpdateImage == "" {
-					return "", errors.New("platform image defined but value is missing")
-				}
 
-				upgradeDefinedMultipleTimes := false
-
-				if object["spec"] != "" {
-
-					if object["spec"].(map[string]interface{})["upstream"] != nil {
-						nextUpstream := object["spec"].(map[string]interface{})["upstream"].(string)
-						if upstream == "" {
-							upstream = nextUpstream
-						} else if upstream != nextUpstream {
-							upgradeDefinedMultipleTimes = true
-						}
-					}
-
-					if object["spec"].(map[string]interface{})["channel"] != nil {
-						nextChannel := object["spec"].(map[string]interface{})["channel"].(string)
-						if channel == "" {
-							channel = nextChannel
-						} else if channel != nextChannel {
-							upgradeDefinedMultipleTimes = true
-						}
-					}
-
-					if object["spec"].(map[string]interface{})["desiredUpdate"] != nil {
-						if object["spec"].(map[string]interface{})["desiredUpdate"].(map[string]interface{})["version"] != nil {
-							nextVersion := object["spec"].(map[string]interface{})["desiredUpdate"].(map[string]interface{})["version"].(string)
-							if version == "" {
-								version = nextVersion
-							} else if version != nextVersion {
-								upgradeDefinedMultipleTimes = true
-							}
-						}
+				if object["spec"].(map[string]interface{})["upstream"] != nil {
+					nextUpstream := object["spec"].(map[string]interface{})["upstream"].(string)
+					if upstream == "" {
+						upstream = nextUpstream
+					} else if upstream != nextUpstream {
+						return "", errors.New("platform image defined more then once with conflicting upstream values")
 					}
 				}
 
-				if upgradeDefinedMultipleTimes {
-					return "", errors.New("platform image defined more then once with conflicting values")
+				if object["spec"].(map[string]interface{})["channel"] != nil {
+					nextChannel := object["spec"].(map[string]interface{})["channel"].(string)
+					if channel == "" {
+						channel = nextChannel
+					} else if channel != nextChannel {
+						return "", errors.New("platform image defined more then once with conflicting channel values")
+					}
 				}
 
-				image, err = r.getImageForVersionFromUpdateGraph(upstream, channel, version)
-				if err != nil {
-					return "", err
+				if object["spec"].(map[string]interface{})["desiredUpdate"] != nil {
+					if object["spec"].(map[string]interface{})["desiredUpdate"].(map[string]interface{})["version"] != nil {
+						nextVersion := object["spec"].(map[string]interface{})["desiredUpdate"].(map[string]interface{})["version"].(string)
+						if version == "" {
+							version = nextVersion
+						} else if version != nextVersion {
+							return "", errors.New("platform image defined more then once with conflicting version values")
+						}
+					}
+					if object["spec"].(map[string]interface{})["desiredUpdate"].(map[string]interface{})["image"] != nil {
+						nextImage := object["spec"].(map[string]interface{})["desiredUpdate"].(map[string]interface{})["image"].(string)
+						if image == "" {
+							image = nextImage
+						} else if image != nextImage {
+							return "", errors.New("platform image defined more then once with conflicting image values")
+						}
+					}
 				}
-				if image == "" {
-					return "", errors.New("unable to find platform image for specified upstream, channel, and version")
-				}
+
+				foundClusterVersionGroupVersion = true
 			default:
 				continue
 			}
 		}
 	}
+
+	if !foundClusterVersionGroupVersion {
+		return "", nil
+	}
+
+	// return early if policy is valid and user provided .Spec.DesiredUpdate.image in ClusterVersion
+	if image != "" {
+		return image, nil
+	}
+
+	// check for all the required variables needed to make http call and retrieve image
+	if upstream == "" || channel == "" || version == "" {
+		return "", errors.New("policy with ClusterVersion must have upstream, channel, and version when image is not provided")
+	}
+	image, err := r.getImageForVersionFromUpdateGraph(upstream, channel, version)
+	if err != nil {
+		return "", err
+	}
+	if image == "" {
+		return "", errors.New("unable to find platform image for specified upstream, channel, and version")
+	}
+
 	return image, nil
 }
 
