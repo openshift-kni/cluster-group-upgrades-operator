b4fd1b9ebf90f6abe9b3d32656b110de526a7d98
Saeid Askari <saskari@redhat.com>
2023-02-06 13:31:44 -0500

    Update golang version to 1.20
    Signed-off-by: Saeid Askari <saskari@redhat.com>

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -3,11 +3,11 @@
 which golangci-lint
 if [ $? -ne 0 ]; then
     echo "Downloading golangci-lint tool"
-    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -d -b $(go env GOPATH)/bin v1.46.1
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -d -b $(go env GOPATH)/bin v1.51.1
 
     if [ $? -ne 0 ]; then
         echo "Install from script failed. Trying go install"
-        go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.1
+        go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.1
         if [ $? -ne 0 ]; then
             echo "Install of golangci-lint failed"
             exit 1
