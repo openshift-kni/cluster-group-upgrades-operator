6ea583840e8044591727d6ca7e231b21171c59aa
Jun Chen <jun@redhat.com>
2023-02-11 22:18:11 -0500

    Update managedClusterResources_test.go

--- controllers/managedClusterResources_test.go
+++ controllers/managedClusterResources_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/go-logr/logr"
-	"github.com/openshift-kni/cluster-group-upgrades-operator/controllers/templates"
 	viewv1beta1 "github.com/open-cluster-management/multicloud-operators-foundation/pkg/apis/view/v1beta1"
+	"github.com/openshift-kni/cluster-group-upgrades-operator/controllers/templates"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
