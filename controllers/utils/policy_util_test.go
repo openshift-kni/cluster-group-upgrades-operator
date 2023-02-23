6c8442f109eeac71e3d13ee97ce86b0e370ede88
Saeid Askari <saskari@redhat.com>
2023-02-22 22:52:08 -0500

    Add ability to wait for `soak duration` after policy becomes compliant
    If a policy has the `soakSeconds` annotation, the controller will wait for the specified
    number of seconds after the first time it observes that the cluster is compliant with
    that policy before moving on from that policy. The controller will set
    `status.status.currentBatchRemediationProgress.clusterName.firstCompliantAt` the first time
    it observes that the cluster is compliant with the policy. In the following reconciliations,
    if the requested number of seconds has passed since firstCompliantAt, and the cluster remains
    compliant with the policy, the controller will move on from that policy.
    
    Signed-off-by: Saeid Askari <saskari@redhat.com>

--- controllers/utils/policy_util_test.go
+++ controllers/utils/policy_util_test.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func TestGetParentPolicyNameAndNamespace(t *testing.T) {
@@ -17,4 +20,38 @@ func TestGetParentPolicyNameAndNamespace(t *testing.T) {
 	res, err = GetParentPolicyNameAndNamespace("default.upgrade.cluster")
 	assert.Equal(t, res[0], "default")
 	assert.Equal(t, res[1], "upgrade.cluster")
+}
+
+func TestShouldSoak(t *testing.T) {
+	// no annotation
+	res, err := ShouldSoak(&unstructured.Unstructured{}, v1.Now())
+	assert.Equal(t, res, false)
+	assert.NoError(t, err)
+
+	// annotation present and soak duration is not over
+	policy := &unstructured.Unstructured{}
+	policy.SetAnnotations(map[string]string{
+		"soakSeconds": "10",
+	})
+	res, err = ShouldSoak(policy, v1.Now())
+	assert.Equal(t, true, res)
+	assert.NoError(t, err)
+
+	// annotation present, firstCompliant is zero
+	res, err = ShouldSoak(policy, v1.Time{})
+	assert.Equal(t, true, res)
+	assert.NoError(t, err)
+
+	// annotation present and soak duration is over
+	firstCompliantAt := time.Now().Add(time.Duration(-11) * time.Second)
+	res, err = ShouldSoak(policy, v1.NewTime(firstCompliantAt))
+	assert.Equal(t, false, res)
+	assert.NoError(t, err)
+
+	// annotation present, soak duration is invalid
+	policy.SetAnnotations(map[string]string{
+		"soakSeconds": "-1",
+	})
+	_, err = ShouldSoak(policy, v1.Now())
+	assert.Error(t, err)
 }
