062638746d2fc3fe29dced1de0cf77a330e1d050
Saeid Askari <saskari@redhat.com>
2023-03-01 15:06:22 -0500

    Fix precache check space not showing error message (#489)
    Signed-off-by: Saeid Askari <saskari@redhat.com>

--- pre-cache/precache.sh
+++ pre-cache/precache.sh
@@ -9,7 +9,6 @@ cp -rf /etc/config /host/tmp/precache/config
 # only check space for OCP upgrade
 if [ -n "$(cat /etc/config/platform.image)" ]; then
     /opt/precache/check_space
-    [ $? -ne 0 ] && echo "not enough space for precaching" > /dev/stderr && exit 17
 fi
 chroot /host /tmp/precache/release
 chroot /host /tmp/precache/olm
