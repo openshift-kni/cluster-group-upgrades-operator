5d64ea8f35ad11dc01ffda65dd18a4159e786cfd
Saeid Askari <saskari@redhat.com>
2023-03-08 11:43:25 -0500

    Rename soaking annotation to `ran.openshift.io/soak-seconds` (#497)
    Signed-off-by: Saeid Askari <saskari@redhat.com>

--- controllers/utils/constants.go
+++ controllers/utils/constants.go
@@ -140,4 +140,4 @@ const (
 
 // SoakAnnotation is the annotation that can be set on policies, which indicates the least number number of seconds
 // which policies should be compliant before the cgu moves on from that policy
-const SoakAnnotation = "soakSeconds"
+const SoakAnnotation = "ran.openshift.io/soak-seconds"
