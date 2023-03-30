ae55c388c7267233366ce2632cf9da58bcefe4fb
Jun Chen <jun@redhat.com>
2023-03-30 14:11:01 -0400

    Dependency update

--- pkg/generated/clientset/versioned/clientset.go
+++ pkg/generated/clientset/versioned/clientset.go
@@ -32,8 +32,7 @@ type Interface interface {
 	ClustergroupupgradesoperatorV1alpha1() clustergroupupgradesoperatorv1alpha1.ClustergroupupgradesoperatorV1alpha1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	clustergroupupgradesoperatorV1alpha1 *clustergroupupgradesoperatorv1alpha1.ClustergroupupgradesoperatorV1alpha1Client
@@ -59,6 +58,10 @@ func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*Clientset, error) {
 	configShallowCopy := *c
+
+	if configShallowCopy.UserAgent == "" {
+		configShallowCopy.UserAgent = rest.DefaultKubernetesUserAgent()
+	}
 
 	// share the transport between all clients
 	httpClient, err := rest.HTTPClientFor(&configShallowCopy)
