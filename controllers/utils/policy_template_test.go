d7073beba2cabbbe5ce9f542fdb352ba8c28971e
Angie Wang <angwang@redhat.com>
2023-04-13 18:17:34 -0400

    Validate hub template functions before copying policies

--- controllers/utils/policy_template_test.go
+++ controllers/utils/policy_template_test.go
@@ -2,9 +2,9 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
-	"github.com/go-logr/logr"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v3"
 	corev1 "k8s.io/api/core/v1"
@@ -17,7 +17,104 @@ func init() {
 	testscheme.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.ConfigMap{})
 }
 
-func TestResolveHubTemplate(t *testing.T) {
+func TestVerifyHubTemplateFunc(t *testing.T) {
+
+	testcases := []struct {
+		name     string
+		input    string
+		expected error
+	}{
+		{
+			name: "Valid hub templates",
+			input: `
+apiVersion: test.openshift.io/v1
+kind: TestResource
+metadata:
+    name: resource-sample
+    namespace: resource-namespace
+spec:
+    test1: '{{hub fromConfigMap "ztp-common" "common-cm" "common-key" hub}}'
+    test2: '{{hub fromConfigMap "" "common-cm" "common-key" hub}}'
+    test3: '{{hub fromConfigMap "ztp-common" "common-cm" (printf "%s-name" .ManagedClusterName) hub}}'
+    test4: '{{hub (printf "%s-name" .ManagedClusterName) | fromConfigMap "ztp-common" "common-cm" hub}}'
+    test5: '{{hub ( printf "%s-name" .ManagedClusterName ) | fromConfigMap "ztp-common" "common-cm" hub}}'
+    test6: '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-common" "common-cm") hub}}'
+    test7: '{{hub ( printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-common"  "common-cm" ) hub}}'
+    test8: 'test-value-{{hub fromConfigMap "ztp-common" "common-cm" (printf "%s-name" .ManagedClusterName) hub}}'
+    test9: '{{hub fromConfigMap "ztp-common" "common-cm" (printf "%s-name" .ManagedClusterName) hub}}-value'
+    test10: |
+        {{hub fromConfigMap "ztp-common" "common-cm" "common-key" hub}}
+    test11: |
+        {{hub fromConfigMap "ztp-common" "common-cm" "common-key" hub}}-value
+    test12:
+        - '{{hub (fromConfigMap "ztp-common" "common-cm" "common-key") | toInt hub}}'
+        - "{{hub (fromConfigMap \"ztp-common\" \"common-cm\" \"common-key\") | toBool hub}}"
+        - '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-common" "common-cm") | base64enc hub}}'
+        - '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-common" "common-cm") | toInt hub}}-{{hub fromConfigMap "ztp-common" "common-cm" "common-key" | toInt hub}}'
+    test13: '{{hub .ManagedClusterName hub}}'
+`,
+			expected: nil,
+		},
+		{
+			name: "Unsupported hub template fromSecret",
+			input: `
+apiVersion: test.openshift.io/v1
+kind: TestResource
+metadata:
+    name: resource-sample
+    namespace: resource-namespace
+spec:
+    test1: '{{hub fromSecret "ztp-common" "common-cm" "common-key" hub}}'
+`,
+			expected: fmt.Errorf("fromSecret: " + PlcHubTmplFuncErr),
+		},
+		{
+			name: "Unsupported Printf in the Name field in fromConfigMap function",
+			input: `
+apiVersion: test.openshift.io/v1
+kind: TestResource
+metadata:
+    name: resource-sample
+    namespace: resource-namespace
+spec:
+    test1: '{{hub fromConfigMap "ztp-common" (printf "%s-data" .ManagedClusterName) "common-key" hub}}'
+`,
+			expected: fmt.Errorf(PlcHubTmplPrinfInNameErr),
+		},
+		{
+			name: "Unsupported Printf in the Namespace field in fromConfigMap function",
+			input: `
+apiVersion: test.openshift.io/v1
+kind: TestResource
+metadata:
+    name: resource-sample
+    namespace: resource-namespace
+spec:
+    test1: '{{hub fromConfigMap ( printf "%s-data" .ManagedClusterName ) "ztp-common"  "common-key" hub}}'
+`,
+			expected: fmt.Errorf(PlcHubTmplPrinfInNsErr),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			var inputYaml interface{}
+			// covert input string to yaml
+			if err := yaml.Unmarshal([]byte(tc.input), &inputYaml); err != nil {
+				t.Errorf("Unexpected error: %v", err.Error())
+			}
+
+			err := VerifyHubTemplateFunctions(inputYaml, "test-policy")
+			if err != nil {
+				assert.ErrorContains(t, err, tc.expected.Error())
+			} else {
+				assert.Equal(t, tc.expected, err)
+			}
+		})
+	}
+}
+
+func TestProcessHubTemplateFunctions(t *testing.T) {
 
 	testcases := []struct {
 		name     string
@@ -51,6 +148,7 @@ spec:
         - "{{hub (fromConfigMap \"ztp-common\" \"common-cm\" \"common-key\") | toBool hub}}"
         - '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-common" "common-cm") | base64enc hub}}'
         - '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-common" "common-cm") | toInt hub}}-{{hub fromConfigMap "ztp-common" "common-cm" "common-key" | toInt hub}}'
+    test13: '{{hub .ManagedClusterName hub}}'
 `,
 			expected: `
 apiVersion: test.openshift.io/v1
@@ -77,46 +175,8 @@ spec:
         - "{{hub (fromConfigMap \"ztp-install\" \"ztp-common.common-cm\" \"common-key\") | toBool hub}}"
         - '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-install" "ztp-common.common-cm") | base64enc hub}}'
         - '{{hub (printf "%s-name" .ManagedClusterName | fromConfigMap "ztp-install" "ztp-common.common-cm") | toInt hub}}-{{hub fromConfigMap "ztp-install" "ztp-common.common-cm" "common-key" | toInt hub}}'
+    test13: '{{hub .ManagedClusterName hub}}'
 `,
-		},
-		{
-			name: "Unsupported hub template fromSecret",
-			input: `
-apiVersion: test.openshift.io/v1
-kind: TestResource
-metadata:
-    name: resource-sample
-    namespace: resource-namespace
-spec:
-    test1: '{{hub fromSecret "ztp-common" "common-cm" "common-key" hub}}'
-`,
-			expected: "fromSecret: " + PlcHubTmplFuncErr,
-		},
-		{
-			name: "Unsupported Printf in the Name field in fromConfigMap function",
-			input: `
-apiVersion: test.openshift.io/v1
-kind: TestResource
-metadata:
-    name: resource-sample
-    namespace: resource-namespace
-spec:
-    test1: '{{hub fromConfigMap "ztp-common" (printf "%s-data" .ManagedClusterName) "common-key" hub}}'
-`,
-			expected: PlcHubTmplPrinfInNameErr,
-		},
-		{
-			name: "Unsupported Printf in the Namespace field in fromConfigMap function",
-			input: `
-apiVersion: test.openshift.io/v1
-kind: TestResource
-metadata:
-    name: resource-sample
-    namespace: resource-namespace
-spec:
-    test1: '{{hub fromConfigMap ( printf "%s-data" .ManagedClusterName ) "ztp-common"  "common-key" hub}}'
-`,
-			expected: PlcHubTmplPrinfInNsErr,
 		},
 	}
 
@@ -133,26 +193,28 @@ spec:
 		r := &TemplateResolver{
 			Client:          fake.NewClientBuilder().WithScheme(testscheme).WithObjects(objs...).Build(),
 			Ctx:             context.TODO(),
-			Log:             logr.Discard(),
 			TargetNamespace: "ztp-install",
 			PolicyName:      "resource-sample-ori",
 			PolicyNamespace: "ztp-common",
 		}
 
 		t.Run(tc.name, func(t *testing.T) {
-			var inputData interface{}
-			if err := yaml.Unmarshal([]byte(tc.input), &inputData); err != nil {
+			var inputYaml interface{}
+			// convert input string to yaml
+			if err := yaml.Unmarshal([]byte(tc.input), &inputYaml); err != nil {
 				t.Errorf("Unexpected error: %v", err.Error())
 			}
 
-			actualResult, err := r.ResolveObjectHubTemplates(inputData)
+			actualResult, err := r.ProcessHubTemplateFunctions(inputYaml)
 			if err != nil {
-				assert.ErrorContains(t, err, tc.expected)
+				t.Errorf("Unexpected error: %v", err.Error())
 			} else {
 				var expectedResult interface{}
+				// convert expected output string to yaml
 				if err := yaml.Unmarshal([]byte(tc.expected), &expectedResult); err != nil {
 					t.Errorf("Unexpected error: %v", err.Error())
 				}
+
 				assert.Equal(t, expectedResult, actualResult)
 			}
 		})
