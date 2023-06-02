a18cdb8cd9469f3c734e47c98413c3fb9590fd63
Irina Mihai <imihai@redhat.com>
2023-06-02 11:24:08 -0400

    OCPBUGS-7389: pre-cache container mounts host's root
    Description:
    - mount only the needed host paths for the pre-cache container with
      the needed privilege access
    - change the check_space script to make the check for host
      instead of the pre-cache container
    - update tests

--- pre-cache/precache.sh
+++ pre-cache/precache.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 set -e
 
+# Setup the environment for running podman.
+mkdir /host/dev
+mknod -m 0666 /host/dev/null c 1 3
+mkdir /host/dev/shm
+/host/usr/bin/mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime tmpfs /host/dev/shm
+
+ln -s usr/bin /host/bin
+
+# Setup the environment for precaching.
 rm -rf /host/tmp/precache
 rm -f /host/tmp/images.txt
 cp -a /opt/precache /host/tmp/
 cp -rf /etc/config /host/tmp/precache/config
-# only check space for OCP upgrade
+
+# Check the available space for the OCP upgrade case.
 if [ -n "$(cat /etc/config/platform.image)" ]; then
     /opt/precache/check_space
 fi
