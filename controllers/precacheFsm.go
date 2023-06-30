f8e5cd6cd4f599cc242cc632886457c6e0a3afe2
Jun Chen <jun@redhat.com>
2023-06-30 17:35:36 -0400

    OCPBUGS-15595: Fix precaching job view cleanup (#600)
    * Fix precaching job view cleanup
    
    * Update 08-errors.yaml
    
    * Update 00-setup.yaml
    
    * Update 00-setup.yaml
    
    * Update 00-setup.yaml

--- controllers/precacheFsm.go
+++ controllers/precacheFsm.go
@@ -155,7 +155,7 @@ func (r *ClusterGroupUpgradeReconciler) precachingFsm(ctx context.Context,
 		}
 		if nextState == PrecacheStateSucceeded {
 			// cleanup for succeeded clusters
-			if r.jobAndViewCleanup(ctx, cluster, backupViews, precacheDeleteTemplates) != nil {
+			if r.jobAndViewCleanup(ctx, cluster, precacheAllViews, precacheDeleteTemplates) != nil {
 				r.Log.Error(err, "[precachingFsm] failed to cleanup for", "cluster", cluster)
 				// skip cluster status transition if cleanup not successful
 				continue
