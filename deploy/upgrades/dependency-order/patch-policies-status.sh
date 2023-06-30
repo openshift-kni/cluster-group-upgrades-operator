8e3f650b942368f0a14b4332eb7459ab45fe0fa5
Jun Chen <jun@redhat.com>
2023-06-30 11:23:31 -0400

    Fix precaching job view cleanup

--- deploy/upgrades/dependency-order/patch-policies-status.sh
+++ deploy/upgrades/dependency-order/patch-policies-status.sh
@@ -13,7 +13,7 @@ http://localhost:8001/apis/policy.open-cluster-management.io/v1/namespaces/$2/po
 curl -k -s -X PATCH -H "Accept: application/json, */*" \
 -H "Content-Type: application/merge-patch+json" \
 http://localhost:8001/apis/policy.open-cluster-management.io/v1/namespaces/$3/policies/policy3-common-ptp-sub-policy/status \
---data '{"status":{"compliant":"NonCompliant","status":[{"clustername":"spoke1","clusternamespace":"spoke2","compliant":"NonCompliant"}, {"clustername":"spoke2","clusternamespace":"spoke2","compliant":"NonCompliant"}, {"clustername":"spoke4","clusternamespace":"spoke4","compliant":"NonCompliant"}]}}'
+--data '{"status":{"compliant":"NonCompliant","status":[{"clustername":"spoke1","clusternamespace":"spoke1","compliant":"NonCompliant"}, {"clustername":"spoke2","clusternamespace":"spoke2","compliant":"NonCompliant"}, {"clustername":"spoke4","clusternamespace":"spoke4","compliant":"NonCompliant"}]}}'
 
 curl -k -s -X PATCH -H "Accept: application/json, */*" \
 -H "Content-Type: application/merge-patch+json" \
