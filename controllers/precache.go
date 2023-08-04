e822acf769891ec1d10235cd44c0c79f8d98ec9f
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-08-04 22:34:09 +0000

    [release-4.12] OCPBUGS-17388: Skip upgrade graph check for templatized cluster version (#630)
    * Skip upgrade graph check for templatized cluster version
    
    * Update controllers/utils/policy_template.go
    
    Co-authored-by: Irina Mihai <imihai@redhat.com>
    
    * Update controllers/validation.go
    
    Co-authored-by: Irina Mihai <imihai@redhat.com>
    
    * Update validation.go
    
    * Update validation.go
    
    ---------
    
    Co-authored-by: Jun Chen <jun@redhat.com>
    Co-authored-by: Irina Mihai <imihai@redhat.com>

--- controllers/precache.go
+++ controllers/precache.go
@@ -120,7 +120,7 @@ func (r *ClusterGroupUpgradeReconciler) extractPrecachingSpecFromPolicies(
 
 	var spec ranv1alpha1.PrecachingSpec
 	for _, policy := range policies {
-		objects, err := r.stripPolicy(policy.Object)
+		objects, err := stripPolicy(policy.Object)
 		if err != nil {
 			return spec, err
 		}
@@ -145,7 +145,7 @@ func (r *ClusterGroupUpgradeReconciler) extractPrecachingSpecFromPolicies(
 	}
 
 	// Get the platform image spec from the policies
-	image, err := r.extractOpenshiftImagePlatformFromPolicies(policies)
+	image, err := r.extractOCPImageFromPolicies(policies)
 	if err != nil {
 		return *new(ranv1alpha1.PrecachingSpec), err
 	}
@@ -160,7 +160,7 @@ func (r *ClusterGroupUpgradeReconciler) extractPrecachingSpecFromPolicies(
 // returns: []interface{} - list of the underlying objects in the policy
 //
 //	error
-func (r *ClusterGroupUpgradeReconciler) stripPolicy(
+func stripPolicy(
 	policyObject map[string]interface{}) ([]map[string]interface{}, error) {
 
 	var objects []map[string]interface{}
