bfecf7e7329955fbf0a3e18e0a3aa11550024708
Jun Chen <jun@redhat.com>
2023-09-08 08:43:24 -0400

    Remove leftover logic for polling

--- controllers/managedclusterForCGU_controller.go
+++ controllers/managedclusterForCGU_controller.go
@@ -22,7 +22,6 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
@@ -90,7 +89,7 @@ func (r *ManagedClusterForCguReconciler) Reconcile(ctx context.Context, req ctrl
 	// Stop creating UOCR if ztp of this cluster is done already
 	if _, found := managedCluster.Labels[ztpDoneLabel]; found {
 		r.Log.Info("ZTP for the cluster has completed. "+ztpDoneLabel+" label found.", "Name", managedCluster.Name)
-		return ctrl.Result{}, nil
+		return doNotRequeue(), nil
 	}
 
 	clusterGroupUpgrade := &ranv1alpha1.ClusterGroupUpgrade{}
@@ -102,13 +101,13 @@ func (r *ManagedClusterForCguReconciler) Reconcile(ctx context.Context, req ctrl
 	} else {
 		// clusterGroupUpgrade for this cluster already exists, stop reconcile
 		r.Log.Info("clusterGroupUpgrade found", "Name", clusterGroupUpgrade.Name, "Namespace", clusterGroupUpgrade.Namespace)
-		return ctrl.Result{}, nil
+		return doNotRequeue(), nil
 	}
 
 	// clusterGroupUpgrade CR doesn't exist
 	availableCondition := meta.FindStatusCondition(managedCluster.Status.Conditions, clusterv1.ManagedClusterConditionAvailable)
 	if availableCondition == nil {
-		return requeueNotReadyCluster(managedCluster), nil
+		return r.handleNotReadyCluster(managedCluster), nil
 	} else if availableCondition.Status == metav1.ConditionTrue {
 		// cluster is ready
 		r.Log.Info("cluster is ready", "Name", managedCluster.Name)
@@ -132,17 +131,15 @@ func (r *ManagedClusterForCguReconciler) Reconcile(ctx context.Context, req ctrl
 		}
 	} else {
 		// availableCondition is false or unknown, cluster is not ready
-		return requeueNotReadyCluster(managedCluster), nil
+		return r.handleNotReadyCluster(managedCluster), nil
 	}
 
-	return ctrl.Result{}, nil
+	return doNotRequeue(), nil
 }
 
-func requeueNotReadyCluster(managedCluster *clusterv1.ManagedCluster) reconcile.Result {
-	if time.Since(managedCluster.CreationTimestamp.Time) > 2*time.Hour {
-		return requeueWithLongInterval()
-	}
-	return requeueWithLongInterval()
+func (r *ManagedClusterForCguReconciler) handleNotReadyCluster(managedCluster *clusterv1.ManagedCluster) reconcile.Result {
+	r.Log.Info("cluster is not ready", "Name", managedCluster.Name)
+	return doNotRequeue()
 }
 
 // sort map[string]int by value in ascending order, return sorted keys
