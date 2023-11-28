10d2befae2cbf8e148429a394fc6a2d23ab93e51
Irina Mihai <imihai@redhat.com>
2023-11-28 15:42:37 -0500

    OCPBUGS-20204: Invalid ConfigurationPolicy names
    Description:
    - In the NewSafeResourceName function, TALM truncates the policy name
      if needed. It could happen that the policy name is truncated such
      that the last character is ".". In this situation, the policy name would
      then be concatenated with "-" and a random generated suffix resulting in
      a policy name that follows this model: "policy_name.-suffix".
      This type of name is not accepted.
    - In NewSafeResourceName, when policies are truncated, make sure the
      last character of the obtained base string is either '-' or an
      alphanumerical character.
    - Verify the obtained safe name against the RFC 1123 subdomain regex
      '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*'
      as per K8S
      docs https://kubernetes.io/docs/concepts/overview/working-with-objects/names/

--- controllers/clustergroupupgrade_controller.go
+++ controllers/clustergroupupgrade_controller.go
@@ -1147,7 +1147,10 @@ func (r *ClusterGroupUpgradeReconciler) updateConfigurationPolicyForCopiedPolicy
 		// Update the metadata name of the ConfigurationPolicy.
 		plcTmplDef := plcTmpl.(map[string]interface{})["objectDefinition"].(map[string]interface{})
 		metadata := plcTmplDef["metadata"]
-		r.updateConfigurationPolicyName(clusterGroupUpgrade, metadata)
+		err := r.updateConfigurationPolicyName(clusterGroupUpgrade, metadata)
+		if err != nil {
+			return err
+		}
 
 		// Ensure the resources referenced in the hub template policy exist if applicable
 		plcTmplDefSpec := plcTmplDef["spec"].(map[string]interface{})
@@ -1187,19 +1190,28 @@ func (r *ClusterGroupUpgradeReconciler) updateConfigurationPolicyHubTemplate(
 }
 
 func (r *ClusterGroupUpgradeReconciler) updateConfigurationPolicyName(
-	clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, metadata interface{}) {
+	clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, metadata interface{}) error {
 
 	metadataContent := metadata.(map[string]interface{})
 	name := utils.GetResourceName(clusterGroupUpgrade, metadataContent["name"].(string))
-	safeName := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxPolicyNameLength, 0)
+	safeName, err := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxPolicyNameLength, 0)
+	if err != nil {
+		return err
+	}
+
 	metadataContent["name"] = safeName
+	return nil
 }
 
 func (r *ClusterGroupUpgradeReconciler) createNewPolicyFromStructure(
 	ctx context.Context, clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, policy *unstructured.Unstructured) error {
 
 	name := policy.GetName()
-	safeName := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxPolicyNameLength, len(policy.GetNamespace())+1)
+	safeName, err := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxPolicyNameLength, len(policy.GetNamespace())+1)
+	if err != nil {
+		return err
+	}
+
 	policy.SetName(safeName)
 	if err := controllerutil.SetControllerReference(clusterGroupUpgrade, policy, r.Scheme); err != nil {
 		return err
@@ -1210,7 +1222,7 @@ func (r *ClusterGroupUpgradeReconciler) createNewPolicyFromStructure(
 		Kind:    "Policy",
 		Version: "v1",
 	})
-	err := r.Client.Get(ctx, client.ObjectKey{
+	err = r.Client.Get(ctx, client.ObjectKey{
 		Name:      safeName,
 		Namespace: clusterGroupUpgrade.Namespace,
 	}, existingPolicy)
@@ -1237,7 +1249,10 @@ func (r *ClusterGroupUpgradeReconciler) createNewPolicyFromStructure(
 func (r *ClusterGroupUpgradeReconciler) ensureBatchPlacementRule(ctx context.Context, clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, policyName string, managedPolicy *unstructured.Unstructured) (string, error) {
 
 	name := utils.GetResourceName(clusterGroupUpgrade, managedPolicy.GetName()+"-placement")
-	safeName := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxObjectNameLength, 0)
+	safeName, err := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxObjectNameLength, 0)
+	if err != nil {
+		return "", err
+	}
 	pr := r.newBatchPlacementRule(clusterGroupUpgrade, policyName, safeName, name)
 
 	if err := controllerutil.SetControllerReference(clusterGroupUpgrade, pr, r.Scheme); err != nil {
@@ -1251,7 +1266,7 @@ func (r *ClusterGroupUpgradeReconciler) ensureBatchPlacementRule(ctx context.Con
 		Version: "v1",
 	})
 
-	err := r.Client.Get(ctx, client.ObjectKey{
+	err = r.Client.Get(ctx, client.ObjectKey{
 		Name:      safeName,
 		Namespace: clusterGroupUpgrade.Namespace,
 	}, foundPlacementRule)
@@ -1406,7 +1421,10 @@ func (r *ClusterGroupUpgradeReconciler) ensureBatchPlacementBinding(
 	ctx context.Context, clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, policyName, placementRuleName string, managedPolicy *unstructured.Unstructured) error {
 
 	name := utils.GetResourceName(clusterGroupUpgrade, managedPolicy.GetName()+"-placement")
-	safeName := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxObjectNameLength, 0)
+	safeName, err := utils.GetSafeResourceName(name, clusterGroupUpgrade, utils.MaxObjectNameLength, 0)
+	if err != nil {
+		return err
+	}
 	// Ensure batch placement bindings.
 	pb := r.newBatchPlacementBinding(clusterGroupUpgrade, policyName, placementRuleName, safeName, name)
 
@@ -1420,7 +1438,7 @@ func (r *ClusterGroupUpgradeReconciler) ensureBatchPlacementBinding(
 		Kind:    "PlacementBinding",
 		Version: "v1",
 	})
-	err := r.Client.Get(ctx, client.ObjectKey{
+	err = r.Client.Get(ctx, client.ObjectKey{
 		Name:      safeName,
 		Namespace: clusterGroupUpgrade.Namespace,
 	}, foundPlacementBinding)
--- controllers/utils/common.go
+++ controllers/utils/common.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"regexp"
 
 	ranv1alpha1 "github.com/openshift-kni/cluster-group-upgrades-operator/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/util/rand"
@@ -31,20 +32,28 @@ func GetMinOf3(number1, number2, number3 int) int {
 }
 
 // GetSafeResourceName returns the safename if already allocated in the map and creates a new one if not
-func GetSafeResourceName(name string, clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, maxLength, spareLength int) string {
+func GetSafeResourceName(name string, clusterGroupUpgrade *ranv1alpha1.ClusterGroupUpgrade, maxLength,
+	spareLength int) (string, error) {
+
+	var err error
 	if clusterGroupUpgrade.Status.SafeResourceNames == nil {
 		clusterGroupUpgrade.Status.SafeResourceNames = make(map[string]string)
 	}
 	safeName, ok := clusterGroupUpgrade.Status.SafeResourceNames[name]
 	if !ok {
-		safeName = NewSafeResourceName(name, clusterGroupUpgrade.GetAnnotations()[NameSuffixAnnotation], maxLength, spareLength)
+		safeName, err =
+			NewSafeResourceName(name, clusterGroupUpgrade.GetAnnotations()[NameSuffixAnnotation],
+				maxLength, spareLength)
+		if err != nil {
+			return "", err
+		}
 		clusterGroupUpgrade.Status.SafeResourceNames[name] = safeName
 	}
-	return safeName
+	return safeName, nil
 }
 
 // NewSafeResourceName creates a safe name to use with random suffix and possible truncation based on limits passed in
-func NewSafeResourceName(name, suffix string, maxLength, spareLength int) string {
+func NewSafeResourceName(name, suffix string, maxLength, spareLength int) (string, error) {
 	if suffix == "" {
 		suffix = rand.String(RandomNameSuffixLength)
 	}
@@ -56,5 +65,23 @@ func NewSafeResourceName(name, suffix string, maxLength, spareLength int) string
 	} else {
 		base = name
 	}
-	return fmt.Sprintf("%s-%s", base, suffix)
+
+	// Make sure base ends in '-' or an alphanumerical character.
+	for !regexp.MustCompile(`^[a-zA-Z0-9-]*$`).MatchString(base[len(base)-1:]) {
+		base = base[:len(base)-1]
+	}
+
+	newSafeResourceName := fmt.Sprintf("%s-%s", base, suffix)
+	// Validate the newSafeResourceName matches the required regex as per
+	// https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
+	match, err := regexp.MatchString(RFC1123MetadataRegex, newSafeResourceName)
+	if err != nil {
+		return "", err
+	}
+	if !match {
+		return "", fmt.Errorf("Safe resource name \"%s\" for \"%s\" does not match regex '%s'",
+			newSafeResourceName, name, RFC1123MetadataRegex)
+	}
+
+	return newSafeResourceName, nil
 }
