f90975ef36ee68cc94a81cbd5c40113459aba0f2
Saeid Askari <saskari@redhat.com>
2024-04-28 20:50:49 -0400

    CNF-11733: Add CGUIBU API definition
    This update introduces api definition for ClusterGroupImageBasedUpgrade
    CGUIBU is used to upgrade a group of clusters using IBU and TALM
    
    Signed-off-by: Saeid Askari <saskari@redhat.com>

---
+++ pkg/api/clustergroupimagebasedupgrades/v1alpha1/register.go
@@ -0,0 +1,39 @@
+package v1alpha1
+
+// +kubebuilder:object:generate=true
+import (
+	"github.com/openshift-kni/cluster-group-upgrades-operator/pkg/api/clustergroupimagebasedupgrades"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// SchemeGroupVersion is group version used to register these objects
+var SchemeGroupVersion = schema.GroupVersion{Group: clustergroupimagebasedupgrades.GroupName, Version: "v1alpha1"}
+
+// Kind takes an unqualified kind and returns back a Group qualified GroupKind
+func Kind(kind string) schema.GroupKind {
+	return SchemeGroupVersion.WithKind(kind).GroupKind()
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+var (
+	// SchemeBuilder initializes a scheme builder
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	// AddToScheme is a global function that registers this API group & version to a scheme
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+// Adds the list of known types to Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&ClusterGroupImageBasedUpgrade{},
+		&ClusterGroupImageBasedUpgradeList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
